dbade7d23744afbe1f2debee3378b86f045cf0aa
Marwan <marwanmo475@gmail.com>
2026-03-24 10:28:45 -0400

    starting ipc work

---
+++ Kernel/Layers/Logic/InterProcessComunicationManager.cpp
@@ -0,0 +1,31 @@
+/**
+ * File: InterProcessComunicationManager.cpp
+ * Author: Marwan Mostafa
+ * Description: Inter-process communication manager implementation.
+ */
+
+#include "InterProcessComunicationManager.hpp"
+
+/**
+ * Function: InterProcessComunicationManager::InterProcessComunicationManager
+ * Description: Constructs inter-process communication manager.
+ * Parameters:
+ *   None
+ * Returns:
+ *   InterProcessComunicationManager - Constructed manager instance.
+ */
+InterProcessComunicationManager::InterProcessComunicationManager()
+{
+}
+
+/**
+ * Function: InterProcessComunicationManager::~InterProcessComunicationManager
+ * Description: Destroys inter-process communication manager.
+ * Parameters:
+ *   None
+ * Returns:
+ *   void - No return value.
+ */
+InterProcessComunicationManager::~InterProcessComunicationManager()
+{
+}
---
+++ Kernel/Layers/Logic/InterProcessComunicationManager.hpp
@@ -0,0 +1,14 @@
+/**
+ * File: InterProcessComunicationManager.hpp
+ * Author: Marwan Mostafa
+ * Description: Inter-process communication manager declarations.
+ */
+
+#pragma once
+
+class InterProcessComunicationManager
+{
+public:
+    InterProcessComunicationManager();
+    ~InterProcessComunicationManager();
+};
